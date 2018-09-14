400456fd7df7bd559937dd26f0cc2bbc12032a40
Aphaphon Jaruthanaset <pui.apahaphon@gmail.com>
2018-09-14 19:54:32 +0700

    api book

---
+++ demoapi/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demoapi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoapi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+         IEnumerable<Book> books = new List<Book>()
+         {
+             new Book{ id = "b01", name = "b01", price = 100 },
+             new Book{ id = "b02", name = "b02", price = 200 },
+             new Book{ id = "b03", name = "b03", price = 300 },
+         };
+
+        //getรายการหนังสือทั้งหมด
+        [HttpGet]
+        public IEnumerable<Book> GetAllBook()
+        {
+            return books;
+        }
+
+        //ดึงเฉพาะหนังสือจากชื่อหนังสือ
+        //เพิ่มหนังสือใหม่
+        //แก้ไขหนังสือ
+        //ลบหนังสือจากรหัสหนังสือ
+    }
+}
---
+++ demoapi/Models/Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace demoapi.Models
+{
+    public class Book
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+
+}
